6783d63bb884be6c4ddd878129d7261993c90008
Michelangelo Altamore <michelangelo@altamore.org>
2009-08-31 09:18:12 +0200

    added some behaviour for random images

--- content/javascripts/behaviour.js
+++ content/javascripts/behaviour.js
@@ -1,19 +1,24 @@
-function random_image_index() {
-  const MAX_IMAGE_INDEX = 4;
-  return Math.floor(Math.random()*(MAX_IMAGE_INDEX))
+function random_image_index(current_index) {
+  index = function() { 
+    const MAX_IMAGE_INDEX = 4;
+    return Math.floor(Math.random() * MAX_IMAGE_INDEX) 
+  };
+  
+  random_number = index();
+  while (random_number == current_index)
+     random_number = index();
+  return random_number;
 }
 
+
 $(function() {
-  $.preloadCssImages();
-  
-  // get the current image1 index
-  // get the current image2 index  
-  // generate a random number for image1 such as is different from image1 and image2 index
-   $('body').click(function() {
-     current_left_image = $('div#splash').css('background-image');
-     $('div#splash').css('background-image', current_left_image.replace(/splash_left_\d/, "splash_left_" + random_image_index()));
+    current_left_image = $('div#splash').css('background-image');
+    current_left_index = current_left_image.match(/splash_left_(\d)/)[1]
+    $('div#splash').css('background-image', current_left_image.replace(/splash_left_\d/, "splash_left_" + random_image_index(current_left_index)));
     
-    current_right_image = $('div#splash span').css('background-image')
-    $('div#splash span').css('background-image', current_right_image.replace(/splash_right_\d/, "splash_right_" + random_image_index()));
-  });
+    current_right_image = $('div#splash span').css('background-image');
+    current_right_index = current_right_image.match(/splash_right_(\d)/)[1]
+    $('div#splash span').css('background-image', current_right_image.replace(/splash_right_\d/, "splash_right_" + random_image_index(current_right_index)));
+  
+  $.preloadCssImages();
 });
